5b76ac6df123c544845643f8b2812eb39de3a518
Andy <andy8805232@gmail.com>
2025-12-01 19:24:31 +0800

    123

---
+++ devops.sh
@@ -0,0 +1,159 @@
+#!/bin/bash
+
+sh_name=andydevopsapp   # sh 名字
+github_user=andy-devops # GitHub 用户名或组织名
+
+load_fun_common() {
+	tmp_file=$(mktemp)
+	curl -sSL https://tool.hdyauto.qzz.io/common/common?$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | cut -c1-16) -o "$tmp_file"
+	. "$tmp_file"
+	rm -f "$tmp_file"
+}
+
+load_fun_common
+
+# --------------- 远程部署调用 --------------- #
+remote_deploy() {
+	github_repo_name="$1"   # 仓库名
+	github_repo_branch="$2" # 分支名，例如 main 或 master
+	github_path="$3"        # 仓库子目录
+	install_dir="$4"        # 安装目录
+	echo "最终安装目录是：$install_dir"
+
+	# 下载脚本到变量
+	local script_content=$(curl -sSL tool.hdyauto.qzz.io/common/fun_devops.sh?$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | cut -c1-16))
+
+	# 写入临时文件
+	local tmp_script=$(mktemp)
+	echo "$script_content" >"$tmp_script"
+	chmod +x "$tmp_script"
+
+	# 导入脚本到当前 shell
+	source "$tmp_script"
+
+	if declare -f deploy >/dev/null 2>&1; then
+		deploy "$sh_name" "$github_user" "$github_repo_name" "$github_path" "$github_repo_branch" "$install_dir"
+	else
+		echo "❌ 远程脚本中没有定义 deploy"
+	fi
+
+	rm -f "$tmp_script"
+
+}
+
+# --------------- 本地部署测试调用 --------------- #
+local_deploy() {
+	github_repo_name="$1"   # 仓库名
+	github_repo_branch="$2" # 分支名，例如 main 或 master
+	github_path="$3"        # 仓库子目录
+	install_dir="$4"        # 安装目录
+	echo "最终安装目录是：$install_dir"
+
+	# 导入脚本到当前 shell
+	source ../devopstool/common/fun_devops.sh
+
+	# 假设远程脚本里定义了函数叫 deploy
+	# 现在就可以直接调用：
+	if declare -f deploy >/dev/null 2>&1; then
+		deploy "$sh_name" "$github_user" "$github_repo_name" "$github_path" "$github_repo_branch" "$install_dir"
+	else
+		echo "❌ 远程脚本中没有定义 deploy"
+	fi
+}
+# --------------- 			 --------------- #
+
+# --------------- 远程安装调用 --------------- #
+remote_setup() {
+	github_repo_name="$1"   # 仓库名
+	github_repo_branch="$2" # 分支名，例如 main 或 master
+	github_path="$3"        # 仓库子目录
+	install_dir="$4"        # 安装目录
+	setup_name="$5"         # 安装名字
+	setup_url="$6"          # 安装地址
+	echo "最终安装目录是：$install_dir"
+
+	# 下载脚本到变量
+	local script_content=$(curl -sSL https://tool.hdyauto.qzz.io/common/fun_devops.sh?$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | cut -c1-16))
+
+	# 写入临时文件
+	local tmp_script=$(mktemp)
+	echo "$script_content" >"$tmp_script"
+	chmod +x "$tmp_script"
+
+	# 导入脚本到当前 shell
+	source "$tmp_script"
+
+	if declare -f setup >/dev/null 2>&1; then
+		setup "$Sh_Name" "$GitHub_User" "$GitHub_Repo_Name" "$GitHub_Path" "$GitHub_Repo_Branch" "$Install_Dir" "$setup_name" "$setup_url"
+	else
+		echo "❌ 远程脚本中没有定义 setup"
+	fi
+
+	rm -f "$tmp_script"
+
+}
+
+# --------------- 本地安装测试调用 --------------- #
+local_setup() {
+	github_repo_name="$1"   # 仓库名
+	github_repo_branch="$2" # 分支名，例如 main 或 master
+	github_path="$3"        # 仓库子目录
+	install_dir="$4"        # 安装目录
+	setup_name="$5"         # 安装名字
+	setup_url="$6"          # 安装地址
+	echo "最终安装目录是：$install_dir"
+
+	# 导入脚本到当前 shell
+	source ../devopstool/common/fun_devops.sh
+
+	if declare -f setup >/dev/null 2>&1; then
+		setup "$Sh_Name" "$GitHub_User" "$GitHub_Repo_Name" "$GitHub_Path" "$GitHub_Repo_Branch" "$Install_Dir" "$setup_name" "$setup_url"
+	else
+		echo "❌ 远程脚本中没有定义 setup"
+	fi
+
+}
+
+#--------------- 			 --------------- #
+
+devops_desktop() {
+	github_repo_name=devops-desktop                      # 仓库名
+	github_repo_branch=main                              # 分支名，例如 main 或 master
+	github_path=pod                                      # 仓库子目录
+	install_dir=$(prepare_install_dir "/home/wkdesktop") # 安装目录
+	echo "最终安装目录是：$install_dir"
+
+	remote_deploy "$github_repo_name" "$github_repo_branch" "$github_path"
+}
+
+setup_desktop() {
+	github_repo_name=devops-desktop                      # 仓库名
+	github_path=pod                                      # 仓库子目录
+	github_repo_branch=main                              # 分支名，例如 main 或 master
+	install_dir=$(prepare_install_dir "/home/wkdesktop") # 安装目录
+	setup_file_name="andy.sh"                            # 安装文件名
+	setup_url="devopsandy.hdyauto.qzz.io/menu/setup"
+
+	remote_setup "$github_repo_name" "$github_repo_branch" "$github_path" "$install_dir" "$setup_file_name" "$setup_url"
+}
+
+devops_gitlab() {
+	github_repo_name=devops-gitlab                      # 仓库名
+	github_repo_branch=main                             # 分支名，例如 main 或 master
+	github_path=pod                                     # 仓库子目录
+	install_dir=$(prepare_install_dir "/home/wkgitlab") # 安装目录
+	echo "最终安装目录是：$install_dir"
+
+	remote_deploy "$github_repo_name" "$github_repo_branch" "$github_path"
+}
+
+setup_gitlab() {
+	github_repo_name=devops-gitlab                      # 仓库名
+	github_path=pod                                     # 仓库子目录
+	github_repo_branch=main                             # 分支名，例如 main 或 master
+	install_dir=$(prepare_install_dir "/home/wkgitlab") # 安装目录
+	setup_file_name="andy.sh"                           # 安装文件名
+	setup_url="devopsandy.hdyauto.qzz.io/menu/setup"
+
+	remote_setup "$github_repo_name" "$github_repo_branch" "$github_path" "$install_dir" "$setup_file_name" "$setup_url"
+}
--- devops/devops_desktop.sh
+++ devops/devops_desktop.sh
@@ -21,48 +21,48 @@ Install_Dir=$(prepare_install_dir "/home/wkdesktop") # 安装目录
 
 echo "最终安装目录是：$Install_Dir"
 
-# --------------- 远程测试调用 --------------- #
-remote_deploy() {
+# # --------------- 远程测试调用 --------------- #
+# remote_deploy() {
 
-	# 下载脚本到变量
-	local script_content=$(curl -sSL tool.hdyauto.qzz.io/common/fun_devops.sh?$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | cut -c1-16))
+# 	# 下载脚本到变量
+# 	local script_content=$(curl -sSL tool.hdyauto.qzz.io/common/fun_devops.sh?$(openssl rand -base64 12 | tr -dc 'a-zA-Z0-9' | cut -c1-16))
 
-	# 写入临时文件
-	local tmp_script=$(mktemp)
-	echo "$script_content" >"$tmp_script"
-	chmod +x "$tmp_script"
-
-	# 导入脚本到当前 shell
-	source "$tmp_script"
-
-	if declare -f deploy >/dev/null 2>&1; then
-		deploy "$Sh_Name" "$GitHub_User" "$GitHub_Repo_Name" "$GitHub_Path" "$GitHub_Repo_Branch" "$Install_Dir"
-	else
-		echo "❌ 远程脚本中没有定义 deploy"
-	fi
-
-	rm -f "$tmp_script"
-
-}
-# remote_deploy
-
-# --------------- 			 --------------- #
-
-# # --------------- 本地测试调用 --------------- #
-# local_deploy() {
+# 	# 写入临时文件
+# 	local tmp_script=$(mktemp)
+# 	echo "$script_content" >"$tmp_script"
+# 	chmod +x "$tmp_script"
 
 # 	# 导入脚本到当前 shell
-# 	source ./fun_devops.sh
+# 	source "$tmp_script"
 
-# 	# 假设远程脚本里定义了函数叫 deploy
-# 	# 现在就可以直接调用：
 # 	if declare -f deploy >/dev/null 2>&1; then
 # 		deploy "$Sh_Name" "$GitHub_User" "$GitHub_Repo_Name" "$GitHub_Path" "$GitHub_Repo_Branch" "$Install_Dir"
 # 	else
 # 		echo "❌ 远程脚本中没有定义 deploy"
 # 	fi
+
+# 	rm -f "$tmp_script"
+
 # }
+# remote_deploy
 
-# local_deploy
+# --------------- 			 --------------- #
 
-# # --------------- 			 --------------- #
+# --------------- 本地测试调用 --------------- #
+local_deploy() {
+
+	# 导入脚本到当前 shell
+	source ../../devopstool/common/fun_devops.sh
+
+	# 假设远程脚本里定义了函数叫 deploy
+	# 现在就可以直接调用：
+	if declare -f deploy >/dev/null 2>&1; then
+		deploy "$Sh_Name" "$GitHub_User" "$GitHub_Repo_Name" "$GitHub_Path" "$GitHub_Repo_Branch" "$Install_Dir"
+	else
+		echo "❌ 远程脚本中没有定义 deploy"
+	fi
+}
+
+local_deploy
+
+# --------------- 			 --------------- #
